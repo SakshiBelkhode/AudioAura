a29d321e65480d1602b2679fab7c8bd97b753b89
SakshiBelkhode <126399972+SakshiBelkhode@users.noreply.github.com>
2026-04-05 20:18:22 +0530

    Changes successfully

--- AudioAura/backend/src/app.js
+++ AudioAura/backend/src/app.js
@@ -7,11 +7,13 @@ const musicRoutes = require('./routes/music.routes');
 
 
 const app = express();
-app.use(express.json());
-app.use(cookieParser());
-const cors = require('cors');
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
-
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://audioaura-b.onrender.com'
+  ],
+  credentials: true
+}));
 app.use('/api/auth', authRoutes);
 app.use('/api/music', musicRoutes);
 
